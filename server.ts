ef49215432d87fad09a824035574571f3d95808a
Sloan <gwaltneys2@winthrop.edu>
2018-04-19 19:10:28 -0400

    added mongoose connection

--- server.ts
+++ server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import {devEnv} from './config/env'
 
 class Server {
   app = express()
   constructor(){
+    this.config()
+  }
 
+  config(){
+    let mongooseStr : string = devEnv ? devEnv.mongooseURI : ''
+    mongoose.connect(mongooseStr, (err) => {
+      if (err) console.log('Database Connection Error ' + err)
+      else console.log('Connected To Database')
+    })
   }
 }
 
