3f72a2c19b2b746ba981b76252d028b8db8852c4
Sloan <gwaltneys2@winthrop.edu>
2018-04-20 07:08:50 -0400

    added middleware

--- server.ts
+++ server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+import passport from 'passport'
 import {devEnv} from './config/env'
+import userRouter from './routes/users'
 
 class Server {
   app = express()
   constructor(){
     this.config()
+    this.routes()
   }
 
   config(){
@@ -14,6 +19,20 @@ class Server {
       if (err) console.log('Database Connection Error ' + err)
       else console.log('Connected To Database')
     })
+
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    let cookieKey : string = devEnv ? devEnv.cookieKey : ''
+    this.app.use(cookieSession({
+      name: 'session',
+      keys: [cookieKey]
+    }))
+
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
+  }
+
+  routes(){
+    this.app.use('/users', userRouter)
   }
 }
 
