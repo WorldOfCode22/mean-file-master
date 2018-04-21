68cf601d2b350a1cf8388c5b21adedc00c6b260c
Sloan <gwaltneys2@winthrop.edu>
2018-04-20 22:08:33 -0400

    refactor for datastore

--- models/user.ts
+++ models/user.ts
@@ -1,15 +1,18 @@
 import {Document, Schema, Model, model} from 'mongoose'
+import { IDataStore } from './data-store';
 
 interface IUser {
   username?: string, 
-  password?: string
+  password?: string,
+  dataStores: string[]
 }
 
 export interface IUserModel extends IUser, Document {}
 
 export let UserSchema : Schema = new Schema({
   username: String,
-  password: String
+  password: String,
+  dataStores: Array
 })
 
 export const User: Model<IUserModel> = model<IUserModel>('user', UserSchema)
