4f2a324ef22ff1835fdfc28060e17b1112038491
Sloan <gwaltneys2@winthrop.edu>
2018-04-20 23:35:42 -0400

    added cors header to be removed later

--- routes/data-store.ts
+++ routes/data-store.ts
@@ -5,8 +5,11 @@ import ApiError from "../lib/errors/api-error";
 
 const router = Router()
 
+// @TODO: added file write
 router.post('/', async (req, res) => {
   try {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
+    console.log(req.body)
     // get required fields
     let username = req.body.username
     let token = req.body.token
@@ -34,6 +37,7 @@ router.post('/', async (req, res) => {
     else throw dataStore
   } else throw new ApiError(400, 'Missing required fields', 'MissingRequiredFields')
   } catch (e) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
     if (e instanceof ApiError) {
       res.status(e.statusCode).json({error: e.message})
     } else {
